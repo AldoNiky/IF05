91c607a7ee001957f93cf1389808990a7d0c9444
wissame <wissame@localhost>
2015-01-14 15:11:40 +0100

    essai

--- Taquin/src/Prototype.java
+++ Taquin/src/Prototype.java
@@ -5,8 +5,11 @@ public class Prototype {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//On cree une matrice 3x3
+		int [][] jeu={{1,2,3},
+					{4,0,6},
+					{7,8,5}};
+		//On calcule le nombre de déplacement necessaire au positionnement du 0
 	}
 
 }
