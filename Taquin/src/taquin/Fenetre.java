980fe83aafce5620bea27d4526c6aa733933a937
wissame <wissame@localhost>
2015-01-20 12:06:05 +0100

    version 0.2 jouable avec fleche sans gestion erreur

---
+++ Taquin/src/taquin/Fenetre.java
@@ -0,0 +1,11 @@
+package taquin;
+
+import javax.swing.JFrame;
+
+public class Fenetre extends JFrame {
+
+	public Fenetre (){
+		setVisible(true);
+		this.setSize(100, 100);
+	}
+}
--- Taquin/src/taquin/Test.java
+++ Taquin/src/taquin/Test.java
@@ -1,13 +1,39 @@
 package taquin;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Scanner;
 
 public class Test {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Taquin t = new Taquin(3);
+		final Taquin t = new Taquin(3);
+		Fenetre f= new Fenetre();
+		f.addKeyListener(new KeyListener(){
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				try{
+				if(e.getKeyCode()==KeyEvent.VK_UP) t.deplacement(0);
+				if(e.getKeyCode()==KeyEvent.VK_DOWN) t.deplacement(1);
+				if(e.getKeyCode()==KeyEvent.VK_LEFT) t.deplacement(2);
+				if(e.getKeyCode()==KeyEvent.VK_RIGHT) t.deplacement(3);
+				System.out.println(t);
+				}catch(ImpossibleMoveException err){System.out.print("Impossible.\n");};
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		System.out.println(t);
-		while(!t.estResolu()) {
+		/*while(!t.estResolu()) {
 			String s = sc.next();
 			try {
 				switch (s) {
@@ -20,8 +46,8 @@ public class Test {
 				System.out.print("Impossible.\n");
 			}
 			System.out.println(t);
-		}
-		System.out.print("Bravo !");
-		sc.close();
+		}*/
+		//System.out.print("Bravo !");
+		//sc.close();
 	}
 }
